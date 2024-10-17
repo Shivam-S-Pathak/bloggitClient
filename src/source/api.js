43240ecb65db18e42a3fd3509ba7bdd2850c4e81
Shivam Pathak <shivampathak5042.com>
2024-10-17 20:21:37 +0530

    update

--- src/source/api.js
+++ src/source/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { API_MESSAGES, SERVICE_URLS } from "../constants/config.js";
-const API_URL = "http://bloggit-server.vercel.app/"; // Use http if SSL is not set up
+const API_URL =
+  "https://bloggit-server-57gp5dmkb-shivam-pathaks-projects-3418c177.vercel.app"; // Use http if SSL is not set up
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
